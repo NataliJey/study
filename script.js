d151180243ae2b53fd6ff72f545950484aa0c5b1
NataliJey <Natali23160@yandex.ru>
2022-04-20 08:52:28 +0500

    Script for project 10

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ $(function () {
     let $ingredientsList = $('.ingredients-list');
     let url = 'https://api.allorigins.win/raw?url=https://vk.com/doc387129635_634241610?hash=19bjB2vcojacTm6MAPQNbjxkUK4bO9wBE7SLALB9ZU0&dl=U4IvZr0mwWAMO5x2DLNVJVVjTcz2ZOKD9QM5u2N4JWk';
 
-$.getJSON(url, function(json) {
-         for (let i = 0; i < json.length; i++) {
-             addIngredient(json[i]);
-         }
-});
+    $.getJSON(url, function(json) {
+             for (let i = 0; i < json.length; i++) {
+                 addIngredient(json[i]);
+             }
+    });
 
     function addIngredient(item) {
         let $ingredient = $(`
