28ac4a3d2337760def1245425c4c4bb1a6b7964a
NataliJey <Natali23160@yandex.ru>
2022-04-20 14:55:39 +0500

    Script for project 11

--- script.js
+++ script.js
@@ -2,19 +2,37 @@ $(function () {
 
     let $ingredientsList = $('.ingredients-list');
     let url = 'https://api.allorigins.win/raw?url=https://vk.com/doc387129635_634241610?hash=19bjB2vcojacTm6MAPQNbjxkUK4bO9wBE7SLALB9ZU0&dl=U4IvZr0mwWAMO5x2DLNVJVVjTcz2ZOKD9QM5u2N4JWk';
+    let $itemImage ;
+    let $cell = $('.cell');
+    let $item ;
 
     $.getJSON(url, function(json) {
              for (let i = 0; i < json.length; i++) {
                  addIngredient(json[i]);
              }
+        console.log(5);
+
+        $itemImage = $('.item-image');
+        $item = $('.item');
+        mousedown();
     });
 
     function addIngredient(item) {
         let $ingredient = $(`
             <div class="item">
-                <img src="${item.image}" alt="${item.title}">
+                <img class="item-image" src="${item.image}" alt="${item.title}">
             </div>
         `)
         $ingredientsList.append($ingredient);
     }
+
+    function mousedown() {
+        $itemImage.mousedown(function () {
+
+        })
+    }
+
+
+
+
 });
