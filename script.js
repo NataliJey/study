326dd76a88576a3d55e4b57a1a9d852c100293f8
NataliJey <Natali23160@yandex.ru>
2022-04-21 18:52:56 +0500

    Script for project 12

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ $(function () {
 
     let $ingredientsList = $('.ingredients-list');
     let url = 'https://api.allorigins.win/raw?url=https://vk.com/doc387129635_634241610?hash=19bjB2vcojacTm6MAPQNbjxkUK4bO9wBE7SLALB9ZU0&dl=U4IvZr0mwWAMO5x2DLNVJVVjTcz2ZOKD9QM5u2N4JWk';
-    let $itemImage ;
-    let $cell = $('.cell');
-    let $item ;
+    let $itemImage;
+    // let $cell = $('.cell');
+    let $item;
 
-    $.getJSON(url, function(json) {
-             for (let i = 0; i < json.length; i++) {
-                 addIngredient(json[i]);
-             }
+    $.getJSON(url, function (json) {
+        for (let i = 0; i < json.length; i++) {
+            addIngredient(json[i]);
+        }
         console.log(5);
 
         $itemImage = $('.item-image');
         $item = $('.item');
         mousedown();
+        // mousemove()
+
     });
 
     function addIngredient(item) {
@@ -22,17 +24,29 @@ $(function () {
             <div class="item">
                 <img class="item-image" src="${item.image}" alt="${item.title}">
             </div>
-        `)
+        `);
         $ingredientsList.append($ingredient);
     }
 
     function mousedown() {
         $itemImage.mousedown(function () {
+            let itemImageCopy = $(`
+            <img class="item-image" src="${this.src}" alt="${this.alt}">`)
+
+            $(`body`).append(itemImageCopy);
+            let qwer = this.getBoundingClientRect()
+
+            console.log(itemImageCopy, qwer.x)
 
         })
     }
-
-
-
+    // Координаты курсора относительно всего документа
+    function mousemove() {
+        $(document).mousemove(function(event){
+            let x = event.pageX;
+            let y = event.pageY;
+            // console.log(x,y);
+        })
+    }
 
 });
